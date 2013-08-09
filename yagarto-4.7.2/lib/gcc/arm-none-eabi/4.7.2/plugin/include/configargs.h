a071ca66b50cb1ef9ba5a0d3afbe351e1f7e7e22
zozo123 <eliaz123@gmail.com>
2013-08-10 00:55:01 +0300

    init

---
+++ yagarto-4.7.2/lib/gcc/arm-none-eabi/4.7.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.7.2/configure --target=arm-none-eabi --prefix=/Users/mfischer/Projects/yagarto/install --disable-shared --disable-nls --disable-threads --disable-libssp --disable-libstdcxx-pch --disable-libmudflap --disable-libgomp -v --enable-languages=c,c++ --enable-interwork --enable-multilib --with-gcc --with-gnu-ld --with-gnu-as --with-dwarf2 --with-newlib --with-headers=../newlib-1.20.0/newlib/libc/include --with-mpc=/Users/mfischer/Projects/yagarto/addontools --with-mpfr=/Users/mfischer/Projects/yagarto/addontools --with-gmp=/Users/mfischer/Projects/yagarto/addontools";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
